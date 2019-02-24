dbda10f26da13edd882a704201267641cdd19cb0
etienne_tripier <etienne.tripier@airbnb.com>
2019-02-23 16:46:32 -0800

    Break out transcoding pipeline from remote

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const dotenv = require('dotenv').config();
+const path = require('path');
+const serverlessWebpack = require('serverless-webpack');
+const webpack = require('webpack');
+const webpackNodeExternals = require('webpack-node-externals');
+
+if (dotenv.error) {
+  throw dotenv.error;
+}
+
+module.exports = {
+  entry: serverlessWebpack.lib.entries,
+  externals: [webpackNodeExternals()],
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['@babel/typescript'],
+            ['env', { targets: { node: '8.10' } }],
+          ],
+        },
+      },
+    ],
+  },
+  output: {
+    filename: '[name].js',
+    libraryTarget: 'commonjs',
+    path: path.join(__dirname, '.webpack'),
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      USER_POOL_PUBLIC_KEYS: process.env.USER_POOL_PUBLIC_KEYS,
+      USER_POOL_URL: JSON.stringify(process.env.USER_POOL_URL),
+    }),
+  ],
+  resolve: {
+    alias: {
+      ':functions': path.resolve(__dirname, './functions'),
+    },
+    extensions: ['.js', '.ts'],
+  },
+  stats: 'minimal',
+  target: 'node',
+};
